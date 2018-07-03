292317aeb45bb369edfe6fdfa46908c3de007856
Daksha Patel <dakshapatel100@gmail.com>
2018-07-03 16:10:52 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,19 +7,36 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-"Write your SQL query Here"
+  "SELECT title, SUM(pledges.amount) FROM projects
+  INNER JOIN pledges ON projects.id = pledges.project_id
+  GROUP BY project_id
+  ORDER BY title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"Write your SQL query Here"
+"SELECT name, age, SUM(pledges.amount)
+FROM users
+INNER JOIN pledges
+ON users.id = pledges.user_id
+GROUP BY user_id
+ORDER BY name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+"SELECT title, SUM(pledges.amount) - funding_goal
+FROM projects
+INNER JOIN pledges
+ON project_id = pledges.project_id
+WHERE SUM(pledges.amount) > projects.funding_goal; "
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"Write your SQL query Here"
+"SELECT name, pledges.amount
+FROM users
+INNER JOIN pledges
+ON users.id = pledges.user_id
+GROUP BY name
+ORDER BY amount; "
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
